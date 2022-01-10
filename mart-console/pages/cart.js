c19dde2ecf02c53676bd244b5f66d616fad8f604
GraniteMask <ratnadeepastronomy@gmail.com>
2022-01-11 04:41:07 +0530

    cart functionality completed

--- mart-console/pages/cart.js
+++ mart-console/pages/cart.js
@@ -38,7 +38,7 @@ function CartScreen() {
             </div>) :
             (
                 <Grid container spacing={1}>
-                    <Grid item md={9} xs={12}>
+                    <Grid item md={8} xs={12}>
                         <TableContainer>
                             <Table>
                                 <TableHead>
@@ -84,12 +84,12 @@ function CartScreen() {
 
                         </TableContainer>
                     </Grid>
-                    <Grid item md={3} xs={12}>
+                    <Grid item md={4} xs={12}>
                         <Card style={{ background: 'rgba(0, 0, 0, 0.1) no-repeat scroll 16px 16px'}}>
                             <List>
                                 <ListItem>
                                     <Typography variant="h2">
-                                        Subtotal ({cartItems.reduce((a, c) => a + c.quantity, 0)} {' '} items) : ${cartItems.reduce((a, c) => a + c.quantity * c.productPrice, 0)}
+                                        Subtotal ({cartItems.reduce((a, c) => a + c.quantity, 0)} {' '} items) : Rs. {cartItems.reduce((a, c) => a + c.quantity * c.productPrice, 0)}
                                     </Typography>
                                 </ListItem>
                                 <ListItem>
