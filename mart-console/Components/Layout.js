e471294d4ada69177710665f81a636534dfe05ba
GraniteMask <ratnadeepastronomy@gmail.com>
2022-01-10 18:23:13 +0530

    Mart-console initiated

---
+++ mart-console/Components/Layout.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Head from 'next/head'
+import NextLink from 'next/link'
+import {AppBar, Typography, Toolbar, Container, Link, ThemeProvider, CssBaseline, Switch, Badge, Button, Menu, MenuItem, Box, IconButton, Drawer, Divider, List, ListItem, ListItemText} from '@material-ui/core'
+
+export default function Layout({title,description,children}) {
+    return (
+        <div>
+            
+        </div>
+    )
+}
