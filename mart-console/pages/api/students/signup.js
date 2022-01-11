56b9ee6d39c8e0e7db33dfd565e6d7095ec9676d
GraniteMask <ratnadeepastronomy@gmail.com>
2022-01-12 03:40:13 +0530

    student schema added

--- mart-console/pages/api/students/signup.js
+++ mart-console/pages/api/students/signup.js
@@ -8,7 +8,7 @@ const handler = nc()
 
 handler.post(async(req, res)=>{
     await db.connect()
-    const newUser = new User({name: req.body.name, email: req.body.email, password: bcrypt.hashSync(req.body.password), isAdmin: false})
+    const newUser = new User({name: req.body.name, email: req.body.email, password: bcrypt.hashSync(req.body.password)})
     const user = await newUser.save()
     await db.disconnect()
     
