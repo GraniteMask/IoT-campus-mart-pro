f3690692e1c60f8f923b383c4c6a0644134238c8
GraniteMask <ratnadeepastronomy@gmail.com>
2022-01-12 03:51:26 +0530

    signup process completed

--- mart-console/pages/api/students/signup.js
+++ mart-console/pages/api/students/signup.js
@@ -1,5 +1,5 @@
 import nc from 'next-connect'
-import User from '../../../models/User'
+import Student from '../../../models/Students'
 import { signToken } from '../../../utils/auth'
 import db from '../../../utils/db'
 import bcrypt from 'bcryptjs'
@@ -8,7 +8,7 @@ const handler = nc()
 
 handler.post(async(req, res)=>{
     await db.connect()
-    const newUser = new User({
+    const newUser = new Student({
         name: req.body.name, 
         email: req.body.email, 
         password: bcrypt.hashSync(req.body.password), 
