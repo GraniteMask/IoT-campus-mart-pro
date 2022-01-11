700969c24da3e72cf94eb3703634180c43def27d
GraniteMask <ratnadeepastronomy@gmail.com>
2022-01-12 03:27:59 +0530

    student schema added

---
+++ mart-console/models/Students.js
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose'
+
+const studentSchema = new mongoose.Schema({
+    name:{type: String, required: true},
+    registrationNumber:{type: String, required: true, unique: true},
+    email:{type: String, required: true, unique: true},
+    password:{type: String, required: true},
+    roomNumber:{type: String, required: true},
+    block:{type: String, required: true},
+    course:{type: String, required: true},
+    year:{type: String, required: true},
+})
+
+const Student = mongoose.models.Student || mongoose.model('Student', studentSchema)
+
+export default Student
