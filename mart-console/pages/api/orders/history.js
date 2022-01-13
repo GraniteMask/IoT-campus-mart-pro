3b4cf49a9e6700fbd4fe03f85636799174a372d3
GraniteMask <ratnadeepastronomy@gmail.com>
2022-01-14 03:41:07 +0530

    placeorder complete

---
+++ mart-console/pages/api/orders/history.js
@@ -0,0 +1,17 @@
+import nc from 'next-connect'
+import Order from '../../../models/Order'
+import { isAuth } from '../../../utils/auth'
+import db from '../../../utils/db'
+
+
+const handler = nc()
+
+handler.use(isAuth)
+
+handler.get(async(req,res)=>{
+    await db.connect()
+    const orders = await Order.find({user: req.user._id})
+    res.send(orders)
+})
+
+export default handler
